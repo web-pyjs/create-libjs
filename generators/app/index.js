a7865523c3f56f2970f2040e74c14d50fef67ef4
reyx <soufiane.nass7@gmail.com>
2019-02-26 03:52:31 +0100

    Add write and install generator function

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,12 +4,59 @@ const chalk = require("chalk");
 const yosay = require("yosay");
 
 module.exports = class extends Generator {
-  prompting() {
+  async prompting() {
     // Have Yeoman greet the user.
     this.log(yosay(`Welcome ${chalk.red("Javascript library")} generator!`));
+    this.answers = await this.prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Your library name",
+        default: this.appname
+      },
+      {
+        type: "input",
+        name: "version",
+        message: "Your library version",
+        default: "0.0.1"
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Your library description",
+        default: "Javascript library"
+      }
+    ]);
   }
 
-  writing() {}
+  writing() {
+    // Copy all file in template to destination
+    this.fs.copy(this.templatePath(), this.destinationPath());
 
-  install() {}
+    // Copy eslintrc config file
+    this.fs.copy(
+      this.templatePath(".eslintrc.json"),
+      this.destinationPath(".eslintrc.json")
+    );
+    // Copy gitignore file
+    this.fs.copy(
+      this.templatePath(".gitignore"),
+      this.destinationPath(".gitignore")
+    );
+
+    // Extends package.json
+
+    const pkgJson = {
+      name: this.answers.name,
+      version: this.answers.version,
+      description: this.answers.description
+    };
+
+    this.fs.extendJSON(this.destinationPath("package.json"), pkgJson);
+  }
+
+  install() {
+    // Install all package with yarn
+    this.yarnInstall();
+  }
 };
