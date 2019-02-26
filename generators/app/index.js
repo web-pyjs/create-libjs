0954f38205f201806f3742e84c2174428e1c2416
reyx <soufiane.nass7@gmail.com>
2019-02-26 04:30:47 +0100

    Create folder for library with if providers else appName

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,18 +2,19 @@
 const Generator = require("yeoman-generator");
 const chalk = require("chalk");
 const yosay = require("yosay");
+const path = require("path");
 
 module.exports = class extends Generator {
+  constructor(args, opts) {
+    super(args, opts);
+    this.argument("appName", { type: String, required: true });
+    this.argument("appPath", { type: String, required: false });
+  }
+
   async prompting() {
     // Have Yeoman greet the user.
     this.log(yosay(`Welcome ${chalk.red("Javascript library")} generator!`));
     this.answers = await this.prompt([
-      {
-        type: "input",
-        name: "name",
-        message: "Your library name",
-        default: this.appname
-      },
       {
         type: "input",
         name: "version",
@@ -30,40 +31,46 @@ module.exports = class extends Generator {
   }
 
   writing() {
+    const { version, description } = this.answers;
+    const { appName, appPath } = this.options;
+    const appPrefix = appPath || appName;
+
     // Copy all file in template to destination
-    this.fs.copyTpl(this.templatePath(), this.destinationPath(), {
-      name: this.answers.name
+    this.fs.copyTpl(this.templatePath(), this.destinationPath(appPrefix), {
+      name: appName
     });
 
     // Copy eslintrc config file
     this.fs.copy(
       this.templatePath(".eslintrc.json"),
-      this.destinationPath(".eslintrc.json")
+      this.destinationPath(path.join(appPrefix, ".eslintrc.json"))
     );
     // Copy gitignore file
     this.fs.copy(
       this.templatePath(".gitignore"),
-      this.destinationPath(".gitignore")
+      this.destinationPath(path.join(appPrefix, ".gitignore"))
     );
 
     // Extends package.json
 
-    const { name, version, description } = this.answers;
-
     const pkgJson = {
-      name,
+      name: appName,
       version,
       description,
-      main: `lib/${name}.cjs.js`,
-      module: `lib/${name}.es.js`,
-      unpkg: `umd/${name}.js`
+      main: `lib/${appName}.cjs.js`,
+      module: `lib/${appName}.es.js`,
+      unpkg: `umd/${appName}.js`
     };
 
-    this.fs.extendJSON(this.destinationPath("package.json"), pkgJson);
+    this.fs.extendJSON(
+      this.destinationPath(path.join(appPrefix, "package.json")),
+      pkgJson
+    );
   }
 
   install() {
     // Install all package with yarn
+    process.chdir(path.join(this.options.appPath || this.options.appName));
     this.yarnInstall();
   }
 };
